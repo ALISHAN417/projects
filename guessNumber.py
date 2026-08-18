ef0352280b2c9b067e693ff51d0240bcfbf1a9f8
student alishan <alishanchaudahary417@gmail.com>
2026-08-18 18:06:12 +0530

    d

---
+++ guessNumber.py
@@ -0,0 +1,19 @@
+#project
+# guess the random number 
+import random
+target = random.randint(1,100)
+
+while True:
+    userChoice = input("guess the target or Quit(Q) :")
+    if( userChoice == "Q"):
+        break
+    userChoice = int(userChoice)
+    if(userChoice == target):
+        print("Success : Correct Guess!!")
+        break
+    elif(userChoice < target):
+        print("your number was too small. Taking a bigger guess..")
+    else:
+        print("your number was too big. Take a smaller guess..")
+        
+    print("---GAME OVER---")
